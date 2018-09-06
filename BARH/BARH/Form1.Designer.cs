9096475a9c36dec232f99a7f793cfdd0316d7d53
Hanif Hamidi <Hanif@Melayu.org.uk>
2018-09-06 16:46:57 +0100

    files

---
+++ BARH/BARH/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace BARH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnBar11 = new System.Windows.Forms.Button();
+            this.btnBar12 = new System.Windows.Forms.Button();
+            this.btnBar13 = new System.Windows.Forms.Button();
+            this.btnBar23 = new System.Windows.Forms.Button();
+            this.btnBar22 = new System.Windows.Forms.Button();
+            this.btnBar21 = new System.Windows.Forms.Button();
+            this.btnBar33 = new System.Windows.Forms.Button();
+            this.btnBar32 = new System.Windows.Forms.Button();
+            this.btnBar31 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnQNext = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1254, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 76);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnBar11
+            // 
+            this.btnBar11.BackColor = System.Drawing.Color.Yellow;
+            this.btnBar11.Location = new System.Drawing.Point(178, 55);
+            this.btnBar11.Name = "btnBar11";
+            this.btnBar11.Size = new System.Drawing.Size(133, 86);
+            this.btnBar11.TabIndex = 1;
+            this.btnBar11.Text = "stigma";
+            this.btnBar11.UseVisualStyleBackColor = false;
+            this.btnBar11.Click += new System.EventHandler(this.btnBar11_Click);
+            // 
+            // btnBar12
+            // 
+            this.btnBar12.BackColor = System.Drawing.Color.Yellow;
+            this.btnBar12.Location = new System.Drawing.Point(337, 55);
+            this.btnBar12.Name = "btnBar12";
+            this.btnBar12.Size = new System.Drawing.Size(133, 86);
+            this.btnBar12.TabIndex = 2;
+            this.btnBar12.Text = "silate";
+            this.btnBar12.UseVisualStyleBackColor = false;
+            this.btnBar12.Click += new System.EventHandler(this.btnBar12_Click);
+            // 
+            // btnBar13
+            // 
+            this.btnBar13.BackColor = System.Drawing.Color.Yellow;
+            this.btnBar13.Location = new System.Drawing.Point(504, 55);
+            this.btnBar13.Name = "btnBar13";
+            this.btnBar13.Size = new System.Drawing.Size(133, 86);
+            this.btnBar13.TabIndex = 3;
+            this.btnBar13.Text = "syncfusion";
+            this.btnBar13.UseVisualStyleBackColor = false;
+            this.btnBar13.Click += new System.EventHandler(this.btnBar13_Click);
+            // 
+            // btnBar23
+            // 
+            this.btnBar23.BackColor = System.Drawing.Color.Yellow;
+            this.btnBar23.Location = new System.Drawing.Point(504, 159);
+            this.btnBar23.Name = "btnBar23";
+            this.btnBar23.Size = new System.Drawing.Size(133, 86);
+            this.btnBar23.TabIndex = 6;
+            this.btnBar23.Text = "FatherTestification";
+            this.btnBar23.UseVisualStyleBackColor = false;
+            this.btnBar23.Click += new System.EventHandler(this.btnBar23_Click);
+            // 
+            // btnBar22
+            // 
+            this.btnBar22.BackColor = System.Drawing.Color.Yellow;
+            this.btnBar22.Location = new System.Drawing.Point(337, 159);
+            this.btnBar22.Name = "btnBar22";
+            this.btnBar22.Size = new System.Drawing.Size(142, 86);
+            this.btnBar22.TabIndex = 5;
+            this.btnBar22.Text = "FatherTeotihuacanSecret";
+            this.btnBar22.UseVisualStyleBackColor = false;
+            this.btnBar22.Click += new System.EventHandler(this.btnBar22_Click);
+            // 
+            // btnBar21
+            // 
+            this.btnBar21.BackColor = System.Drawing.Color.Yellow;
+            this.btnBar21.Location = new System.Drawing.Point(178, 159);
+            this.btnBar21.Name = "btnBar21";
+            this.btnBar21.Size = new System.Drawing.Size(133, 86);
+            this.btnBar21.TabIndex = 4;
+            this.btnBar21.Text = "FatherVikingCertificates";
+            this.btnBar21.UseVisualStyleBackColor = false;
+            this.btnBar21.Click += new System.EventHandler(this.btnBar21_Click);
+            // 
+            // btnBar33
+            // 
+            this.btnBar33.BackColor = System.Drawing.Color.Yellow;
+            this.btnBar33.Location = new System.Drawing.Point(504, 262);
+            this.btnBar33.Name = "btnBar33";
+            this.btnBar33.Size = new System.Drawing.Size(133, 86);
+            this.btnBar33.TabIndex = 9;
+            this.btnBar33.Text = "GermanHitler";
+            this.btnBar33.UseVisualStyleBackColor = false;
+            this.btnBar33.Click += new System.EventHandler(this.btnBar33_Click);
+            // 
+            // btnBar32
+            // 
+            this.btnBar32.BackColor = System.Drawing.Color.Yellow;
+            this.btnBar32.Location = new System.Drawing.Point(337, 262);
+            this.btnBar32.Name = "btnBar32";
+            this.btnBar32.Size = new System.Drawing.Size(133, 86);
+            this.btnBar32.TabIndex = 8;
+            this.btnBar32.Text = "EgyptianFrances";
+            this.btnBar32.UseVisualStyleBackColor = false;
+            this.btnBar32.Click += new System.EventHandler(this.btnBar32_Click);
+            // 
+            // btnBar31
+            // 
+            this.btnBar31.BackColor = System.Drawing.Color.Yellow;
+            this.btnBar31.Location = new System.Drawing.Point(178, 262);
+            this.btnBar31.Name = "btnBar31";
+            this.btnBar31.Size = new System.Drawing.Size(133, 86);
+            this.btnBar31.TabIndex = 7;
+            this.btnBar31.Text = "MalaysianPOV";
+            this.btnBar31.UseVisualStyleBackColor = false;
+            this.btnBar31.Click += new System.EventHandler(this.btnBar31_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.groupBox1.Controls.Add(this.btnQNext);
+            this.groupBox1.Location = new System.Drawing.Point(680, 55);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(172, 293);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
+            // 
+            // btnQNext
+            // 
+            this.btnQNext.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnQNext.Location = new System.Drawing.Point(24, 37);
+            this.btnQNext.Name = "btnQNext";
+            this.btnQNext.Size = new System.Drawing.Size(91, 81);
+            this.btnQNext.TabIndex = 4;
+            this.btnQNext.Text = "&QNext";
+            this.btnQNext.UseVisualStyleBackColor = false;
+            this.btnQNext.Click += new System.EventHandler(this.btnQNext_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(42, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "ProtectingDevice";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(42, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 15);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "SpinningDevice";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(42, 278);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 15);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "HarnessingDevice";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 402);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnBar33);
+            this.Controls.Add(this.btnBar32);
+            this.Controls.Add(this.btnBar31);
+            this.Controls.Add(this.btnBar23);
+            this.Controls.Add(this.btnBar22);
+            this.Controls.Add(this.btnBar21);
+            this.Controls.Add(this.btnBar13);
+            this.Controls.Add(this.btnBar12);
+            this.Controls.Add(this.btnBar11);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "BARH";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnBar11;
+        private System.Windows.Forms.Button btnBar12;
+        private System.Windows.Forms.Button btnBar13;
+        private System.Windows.Forms.Button btnBar23;
+        private System.Windows.Forms.Button btnBar22;
+        private System.Windows.Forms.Button btnBar21;
+        private System.Windows.Forms.Button btnBar33;
+        private System.Windows.Forms.Button btnBar32;
+        private System.Windows.Forms.Button btnBar31;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnQNext;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BARH/BARH/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BARH
+{
+    public partial class Form1 : Form
+    {
+        private List<int> _value = new List<int> { 0, 0, 0 };
+
+        private void fReset()
+        {
+            Random rnd1 = new Random();
+
+            for (int i = 1; i <= 3; i++)
+            {
+                _value[i - 1] = rnd1.Next(1, 4);
+            }
+
+            fUpdateDisplay();
+        }
+
+        private void fUpdateDisplay()
+        {
+            btnBar11.BackColor = Color.Yellow;
+            btnBar12.BackColor = Color.Yellow;
+            btnBar13.BackColor = Color.Yellow;
+            btnBar21.BackColor = Color.Yellow;
+            btnBar22.BackColor = Color.Yellow;
+            btnBar23.BackColor = Color.Yellow;
+            btnBar31.BackColor = Color.Yellow;
+            btnBar32.BackColor = Color.Yellow;
+            btnBar33.BackColor = Color.Yellow;
+
+            switch (_value[0])
+            {
+                case 1:
+                    btnBar11.BackColor = Color.Red;
+                    break;
+                case 2:
+                    btnBar12.BackColor = Color.Red;
+                    break;
+                default:
+                    btnBar13.BackColor = Color.Red;
+                    break;
+            }
+            switch (_value[1])
+            {
+                case 1:
+                    btnBar21.BackColor = Color.Red;
+                    break;
+                case 2:
+                    btnBar22.BackColor = Color.Red;
+                    break;
+                default:
+                    btnBar23.BackColor = Color.Red;
+                    break;
+            }
+            switch (_value[2])
+            {
+                case 1:
+                    btnBar31.BackColor = Color.Red;
+                    break;
+                case 2:
+                    btnBar32.BackColor = Color.Red;
+                    break;
+                default:
+                    btnBar33.BackColor = Color.Red;
+                    break;
+            }
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fReset();
+        }
+
+        private void btnQNext_Click(object sender, EventArgs e)
+        {
+            fReset();
+        }
+
+        private void btnBar11_Click(object sender, EventArgs e)
+        {
+            _value[0] = 1;
+            fUpdateDisplay();
+        }
+
+        private void btnBar12_Click(object sender, EventArgs e)
+        {
+            _value[0] = 2;
+            fUpdateDisplay();
+
+        }
+
+        private void btnBar13_Click(object sender, EventArgs e)
+        {
+            _value[0] = 3;
+            fUpdateDisplay();
+
+        }
+
+        private void btnBar21_Click(object sender, EventArgs e)
+        {
+            _value[1] = 1;
+            fUpdateDisplay();
+
+        }
+
+        private void btnBar22_Click(object sender, EventArgs e)
+        {
+            _value[1] = 2;
+            fUpdateDisplay();
+
+        }
+
+        private void btnBar23_Click(object sender, EventArgs e)
+        {
+            _value[1] = 3;
+            fUpdateDisplay();
+
+        }
+
+        private void btnBar31_Click(object sender, EventArgs e)
+        {
+            _value[2] = 1;
+            fUpdateDisplay();
+
+        }
+
+        private void btnBar32_Click(object sender, EventArgs e)
+        {
+            _value[2] = 2;
+            fUpdateDisplay();
+
+        }
+
+        private void btnBar33_Click(object sender, EventArgs e)
+        {
+            _value[2] = 3;
+            fUpdateDisplay();
+
+        }
+    }
+}
